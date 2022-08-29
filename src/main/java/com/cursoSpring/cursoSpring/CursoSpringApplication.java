38fd3e7b9feb43a0c629a7668c889dfaeb261db8
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-08-29 20:14:40 +0200

    2commit

---
+++ src/main/java/com/cursoSpring/cursoSpring/CursoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cursoSpring.cursoSpring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CursoSpringApplication {
+
+	//public static void main(String[] args) {
+		//SpringApplication.run(CursoSpringApplication.class, args);
+	//}
+
+}
